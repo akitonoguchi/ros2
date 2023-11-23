91a244502603ab7b1f910af9201bad576c6b7c7b
Akito <akitoniko25@gmail.com>
2023-11-23 12:29:40 +0900

    Add a command

---
+++ build/mypkg/prefix_override/sitecustomize.py
@@ -0,0 +1,3 @@
+import sys
+sys.real_prefix = sys.prefix
+sys.prefix = sys.exec_prefix = '/home/akito/ros2_ws/install/mypkg'
---
+++ install/mypkg/lib/python3.8/site-packages/mypkg/talker.py
@@ -0,0 +1,18 @@
+import rclpy                     #ROS2のクライアントのためのライブラリ
+from rclpy.node import Node
+from person_msgs.msg import Person #使う型を変更
+
+rclpy.init()
+node = Node("talker")
+pub = node.create_publisher(Person, "person", 10) #変更
+n = 0
+def cb():
+     global n
+     msg = Person()         #受信するデータの型を変更
+     msg.name = "野口諒人"  #msgファイルに書いた「name」
+     msg.age = n            #msgファイルに書いた「age」
+     pub.publish(msg)
+     n += 1
+
+node.create_timer(0.5, cb)
+rclpy.spin(node)
